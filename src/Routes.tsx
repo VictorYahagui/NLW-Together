8ecfe10eccb1cd527cedd0f469e725f133297ad4
unknown <victoryahagui@gmail.com>
2022-06-09 21:05:25 -0300

    created the CSS for the questions, the admin page and the question hook

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { AdminRoom } from "./pages/AdminRoom";
 import { Home } from "./pages/Home"
 import { NewRoom } from "./pages/NewRoom"
 import { Room } from "./pages/Room";
@@ -11,6 +12,7 @@ export function AppRoutes() {
                 <Route path="/" element={<Home />}></Route>
                 <Route path="/rooms/new" element={<NewRoom />}></Route>
                 <Route path="/rooms/:id" element={<Room />}></Route>
+                <Route path="/admin/rooms/:id" element={<AdminRoom />}></Route>
             </Routes>
         </BrowserRouter>
     );
